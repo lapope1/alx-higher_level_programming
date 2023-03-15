8a60b5788a3f295a9d9fd592ecb1ed8caa81ce58
Ayomiposi Popoola <ayomiposi4joy@gmail.com>
2023-03-15 07:11:27 -0400

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     new_dictionary = a_dictionary.copy()
     for key, value in list(new_dictionary.items()):
         new_dictionary[key] = value * 2
-    return 
+    return new_dictionary
