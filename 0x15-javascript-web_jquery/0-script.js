bd8bf0554ec22157ec25910658c21aa58f56f467
Ayomiposi Popoola <ayomiposi4joy@gmail.com>
2023-08-02 11:05:48 +0100

    Update 0-script.js

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>My Web Page</title>
-</head>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<body>
-    <header>This is the header element</header>
+const header = document.querySelector('header');
 
-    <script>
-        // Find the <header> element using document.querySelector
-        const headerElement = document.querySelector('header');
-
-        // Update the text color of the <header> element to red (#FF0000)
-        headerElement.style.color = '#FF0000';
-    </script>
-</body>
-</html>
-
+header.style.color = '#FF0000';
