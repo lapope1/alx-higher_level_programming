a85b878325ef4b6b66219eee8cc733a627cd78c0
lapope1 <ayomiposi4joy@gmail.com>
2023-08-02 02:13:16 -0700

    update

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Web Page</title>
+</head>
+<body>
+    <header>This is the header element</header>
+
+    <script>
+        // Find the <header> element using document.querySelector
+        const headerElement = document.querySelector('header');
+
+        // Update the text color of the <header> element to red (#FF0000)
+        headerElement.style.color = '#FF0000';
+    </script>
+</body>
+</html>
+
---
+++ 0x15-javascript-web_jquery/app.js
@@ -0,0 +1,9 @@
+// Import jQuery module
+const $ = require('jquery');
+
+// Your jQuery code here
+$(document).ready(function() {
+    // jQuery code goes here
+    console.log("Hello from jQuery!");
+});
+
